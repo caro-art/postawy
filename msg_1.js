f2bf226c9639bbb6915b60b84202f10d76a0d6fa
caro-art <80354123+caro-art@users.noreply.github.com>
2021-03-28 12:53:35 +0200

    Update msg_1.js

--- msg_1.js
+++ msg_1.js
@@ -123,7 +123,7 @@ define(['pipAPI','pipScorer','underscore'], function(APIConstructor, Scorer, _) 
 			blockAttributes_nMiniBlocks : 5,
 			blockCategories_nTrials : 20,
 			blockCategories_nMiniBlocks : 5,
-			blockFirstCombined_nTrials : 20,
+			blockFirstCombined_nTrials : 25,
 			blockFirstCombined_nMiniBlocks : 5,
 			blockSecondCombined_nTrials : 0, //Change to 0 if you want 5 blocks (you would probably want to increase blockFirstCombined_nTrials).
 			blockSecondCombined_nMiniBlocks : 10, 
